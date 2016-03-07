fc628ff8cba7f6b0e116e1dd57c7037e19f3d454
simon <28737164@qq.com>
2016-03-07 17:23:20 +0800

    debug

--- modules/simon/document/src/Http/router.php
+++ modules/simon/document/src/Http/router.php
@@ -1,6 +1,7 @@
 <?php
 $router->get('{cid?}','DocumentController@getIndex')->where(['cid'=>'[1-9][\d]*']);
 $router->get('show/{did}','DocumentController@getShow')->where(['did'=>'[1-9][\d]*']);
+$router->get('test','DocumentController@getTest');
 $router->controllers([
 	'manage/category'=>'Manage\CategoryController',
 	'manage/document'=>'Manage\DocumentController',
