46d4033b6f81f8c839152f8242485cb56375a4ea
simon <28737164@qq.com>
2016-03-08 23:15:29 +0800

    test

--- public/2.php
+++ public/2.php
@@ -1,4 +1,26 @@
 <?php
+
+function a()
+{
+	echo 1;
+	return true;
+}
+
+function b()
+{
+	echo 2;
+	return false;
+}
+
+
+if (a() )
+{
+	echo 'a';
+}
+
+
+exit();
+
 $b = '<script src="abc3210"></script>';
 
 echo htmlspecialchars(htmlspecialchars($b));
