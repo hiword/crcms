fab4fd547d02a3ae911b129cc25ebd079674ac6b
simon <28737164@qq.com>
2016-03-13 22:16:22 +0800

    分离上传配置

--- config/site.php
+++ config/site.php
@@ -3,26 +3,6 @@ return [
 		
 // 		'theme'=>'skin1',
 		
-		'image_upload'=>[
-			'filesize'=>2097152,
-			'extensions'=>['jpg','jpeg','gif','png','bmp'],
-			'checkExtension'=>true,
-			'checkMime'=>true,
-			'rename'=>true,
-		],
-		'file_upload'=>[
-			'filesize'=>2097152,
-			'extensions'=>['rar','zip','gz'],
-			'check_extension'=>true,
-			'check_mime'=>true,
-			'rename'=>true,
-		],
-		'video_upload'=>[
-			'filesize'=>10097152,
-			'extensions'=>['mp3','mp4','mp5','rmvb','avi','wma'],
-			'check_extension'=>true,
-			'check_mime'=>true,
-			'rename'=>true,
-		]
+		
 		
 ];
