6c1fa718bf6c9cff9afd7da220d1801338dc7460
simon <28737164@qq.com>
2016-05-17 16:58:09 +0800

    模型数据编辑

--- modules/simon/model/src/Http/Controllers/Manage/ElementController.php
+++ modules/simon/model/src/Http/Controllers/Manage/ElementController.php
@@ -41,18 +41,59 @@ class ElementController extends Controller
 		return $this->view('index',['bs'=>[1,2]]);
 	}
 	
-	protected function forms($modelIds)
+	protected function forms($modelId,$id = 0)
 	{
-		$forms = [];
-		foreach($modelIds as $id)
+		$model = $this->modelService->find($modelId);
+		$fields = $this->modelService->fields($model);
+		if ($id)
 		{
-			$model = $this->modelService->find($id);
-			$fields = $this->modelService->fields($model);
-			$view = (new Factory($model, $fields,$this->request))->view();
-			$forms = array_merge($view,$forms);
+			$primary = $fields->get($fields->search(function($item,$key){
+				return $item->is_primary !== 2;
+			}))->name;
+			$data = (array)DB::table($model->table_name)->where($primary,$id)->first();
+			
+			//mults
+			$index = $fields->search(function($item,$key){
+				return $item->type === 'Multiselect';
+			});
+			if ($index !== false)
+			{
+				$multField = $fields->get($index);
+				
+				if ($multField->setting->store_type === 'table')
+				{
+					$expression = $multField->setting->store_table;
+					list($table,$fork,$other) = explode(',', $expression);
+					$forkId = explode(':', $fork)[0];
+					$otherId = explode(':', $other)[0];
+					$data[$multField->name] = DB::table($table)->where($forkId,$id)->lists($otherId);
+				}
+				elseif ($multField->setting->store_type === 'field')
+				{
+					$data[$multField->name] = explode(',', $data[$multField->name]);
+				}
+			}
+			
+			
 		}
-		return $forms;  
+		else {
+			$data = [];
+		}
+		return (new Factory($model, $fields,$this->request))->view($data);
 	}
+	
+// 	protected function forms($modelIds,array $data = [])
+// 	{
+// 		$forms = [];
+// 		foreach($modelIds as $id)
+// 		{
+// 			$model = $this->modelService->find($id);
+// 			$fields = $this->modelService->fields($model);
+// 			$view = (new Factory($model, $fields,$this->request))->view($data);
+// 			$forms = array_merge($view,$forms);
+// 		}
+// 		return $forms;  
+// 	}
 	
 	public function getCreate() 
 	{
@@ -60,13 +101,30 @@ class ElementController extends Controller
 		/* 这里面的一块代码，回头需要，使用一个elementService来操作，需要调用 这个fieldClient  */
 		$modelId = 10;
 		$extendId = $this->modelService->beExtend($modelId,$this->request);
-		$forms = $this->forms(array_merge([$modelId],$extendId));
+		
+		$forms = [];
+		foreach(array_merge([$modelId],$extendId) as $id)
+		{
+			$forms = array_merge($this->forms($id),$forms);;
+		}
+// 		$forms = $this->forms(array_merge([$modelId],$extendId));
 		return $this->view('create',['forms'=>$forms,'model_id'=>$modelId,'append_model_ids'=>$extendId]);
 	}
 	
 	public function getEdit()
 	{
+		$id = 1;
+		$modelId = 10;
+		//查找数据
+		$extendId = $this->modelService->beExtend($modelId,$this->request);
 		
+		$forms = [];
+		foreach(array_merge([$modelId],$extendId) as $mid)
+		{
+			$forms = array_merge($this->forms($mid,$id),$forms);;
+		}
+		// 		$forms = $this->forms(array_merge([$modelId],$extendId));
+		return $this->view('edit',['forms'=>$forms,'model_id'=>$modelId,'append_model_ids'=>$extendId,'id'=>$id]);
 	}
 	
 	protected function database($modelId,$insertId = 0)
@@ -76,6 +134,8 @@ class ElementController extends Controller
 		$fields = $this->modelService->fields($model);
 		
 		$this->form->validator(new AdapterElementForm($model, $fields),$this->data[$model->mark]);
+		
+		$this->factory = new Factory($model,$fields,$this->request);
 		
 		return $this->factory->store($this->data[$model->mark],$fields->get($fields->search(function($item,$key){
 			return $item->is_primary !== 2;
