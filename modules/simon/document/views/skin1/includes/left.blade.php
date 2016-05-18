1e93ad47131eb559b12ed4f47b1fbcef7846e62b
simon <28737164@qq.com>
2016-05-18 08:04:29 +0800

    Update left.blade.php
    icons

--- modules/simon/document/views/skin1/includes/left.blade.php
+++ modules/simon/document/views/skin1/includes/left.blade.php
@@ -8,4 +8,14 @@
 		<li><a  target="_blank" href="http://old.crcms.cn/index.php?m=index&s=navigate&cid=52">CRCMS下载</a></li>
 		<li><a href="http://old.crcms.cn" target="_blank">旧版入口</a></li>
 	</ul>
+	<div class="bottom-icons">
+		<p>
+			<a href="###"><i class="fa fa-qq fa-lg"></i></a>
+			<a href="https://github.com/hiword/crcms" target="_blank" class="ml10"><i class="fa fa-github fa-lg"></i></a>
+			<a href="mailto:28737164@qq.com" class="ml10"><i class="fa fa-envelope fa-lg"></i></a>
+		</p>
+		<p>
+			Copyright <!-- © --><i class="fa fa-copyright"></i> 2014-2016
+		</p>
+	</div>
 </div>
