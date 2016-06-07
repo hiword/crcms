91a9281c4102a852fa20d75c75a86a1f7c871080
simon <28737164@qq.com>
2016-06-07 17:29:54 +0800

    rule添加

--- modules/simon/comment/src/Forms/Comment/CommentStoreForm.php
+++ modules/simon/comment/src/Forms/Comment/CommentStoreForm.php
@@ -4,5 +4,27 @@ use Simon\Comment\Forms\CommentForm;
 use App\Forms\Interfaces\FormInterface;
 class CommentStoreForm extends CommentForm implements FormInterface
 {
+	/* 
+	 * (non-PHPdoc)
+	 * @see \App\Forms\Interfaces\FormInterface::getAttr()
+	 * @author simon
+	 */
+	public function getAttr()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* 
+	 * (non-PHPdoc)
+	 * @see \App\Forms\Interfaces\FormInterface::getRule()
+	 * @author simon
+	 */
+	public function getRule()
+	{
+		// TODO Auto-generated method stub
+		return $this->rule;
+	}
+
 	
 }
--- modules/simon/comment/src/Forms/CommentForm.php
+++ modules/simon/comment/src/Forms/CommentForm.php
@@ -4,6 +4,10 @@ use App\Forms\AbsForm;
 abstract class CommentForm extends AbsForm
 {
 	
-	
+	protected $rule = [
+		'outside_id'=>['required','integer'],
+		'outside_type'=>['required'],
+		'content'=>['required'],
+	];
 	
 }
