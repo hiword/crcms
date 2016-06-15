ae30448f6f4b75338b1d5faefec097a9fca636c5
simon <28737164@qq.com>
2016-06-15 23:02:39 +0800

    92db skin

---
+++ public/92db/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
+    <title>Bootstrap 101 Template</title>
+    <!-- Bootstrap -->
+    <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/common/css/global.css" rel="stylesheet">
+    <link href="css/92db.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+ </head>
+<body>
+<div class="db-header">
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">就爱逗B</a>
+	    </div>
+	
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">Link</a></li>
+	        <!-- 
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Action</a></li>
+	            <li><a href="#">Another action</a></li>
+	            <li><a href="#">Something else here</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">Separated link</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">One more separated link</a></li>
+	          </ul>
+	        </li> -->
+	      </ul>
+	      <!-- 
+	      <form class="navbar-form navbar-left" role="search">
+	        <div class="form-group">
+	          <input type="text" class="form-control" placeholder="Search">
+	        </div>
+	        <button type="submit" class="btn btn-default">Submit</button>
+	      </form> -->
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#">登录</a></li>
+	        <li><a href="#">注册</a></li>
+	        <!-- 
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Action</a></li>
+	            <li><a href="#">Another action</a></li>
+	            <li><a href="#">Something else here</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">Separated link</a></li>
+	          </ul>
+	        </li> -->
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+</div>
+<div class="container db-main">
+	<div class="col-md-9 main-left">
+		<article class="article-list mb20">
+			<div class="user-info">
+				<a rel="nofollow" target="_blank" href="/users/27825408/">
+					<img alt="满街｀都是狗" class="img-circle" width="35" src="http://pic.qiushibaike.com/system/avtnew/2782/27825408/medium/20160331225958.jpg">
+				</a>
+				<a rel="nofollow" target="_blank" class="ml5" href="/users/27825408/">
+					依然范特西:
+				</a>
+			</div>
+			<div class="list-content mt20">
+				我老公一直对我好是有原因的。还记得刚结婚没多久，一天晚上起来喝水，在厨房的墙上看见一只蟑螂，我飞起就是一脚，结果蟑没踩中，把墙上的瓷砖踢碎了一块。从那以后老公对我更好了。 
+			</div>
+			<div class="list-status mt20 clearfix">
+				<a href="###"><i class="glyphicon glyphicon-thumbs-up mr5"></i>5255</a>
+				<a href="###"><i class="glyphicon glyphicon-thumbs-down ml10 mr5"></i>3333</a>
+				<a href="###"><i class="glyphicon glyphicon-comment ml10 mr5"></i>3333</a>
+			</div>
+		</article>
+		<article class="article-list mb20">
+			<div class="user-info">
+				<a rel="nofollow" target="_blank" href="/users/27825408/">
+					<img alt="满街｀都是狗" class="img-circle" width="35" src="http://pic.qiushibaike.com/system/avtnew/2782/27825408/medium/20160331225958.jpg">
+				</a>
+				<a rel="nofollow" target="_blank" class="ml5" href="/users/27825408/">
+					依然范特西:
+				</a>
+			</div>
+			<div class="list-content mt20">
+				我老公一直对我好是有原因的。还记得刚结婚没多久，一天晚上起来喝水，在厨房的墙上看见一只蟑螂，我飞起就是一脚，结果蟑没踩中，把墙上的瓷砖踢碎了一块。从那以后老公对我更好了。 
+			</div>
+			<div class="list-status mt20 clearfix">
+				<a href="###"><i class="glyphicon glyphicon-thumbs-up mr5"></i>5255</a>
+				<a href="###"><i class="glyphicon glyphicon-thumbs-down ml10 mr5"></i>3333</a>
+				<a href="###"><i class="glyphicon glyphicon-comment ml10 mr5"></i>3333</a>
+			</div>
+		</article>
+	</div>
+	<div class="col-md-3">Cd</div>
+</div>
+</body>
+</html>
