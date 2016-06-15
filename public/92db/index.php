293803bffb678cdebaffd20129539eaacceb13c7
simon <28737164@qq.com>
2016-06-16 07:01:33 +0800

    92sbskin

--- public/92db/index.php
+++ public/92db/index.php
@@ -20,7 +20,7 @@
 <body>
 <div class="db-header">
 	<nav class="navbar navbar-default">
-	  <div class="container-fluid">
+	  <div class="container"><!-- -fluid -->
 	    <!-- Brand and toggle get grouped for better mobile display -->
 	    <div class="navbar-header">
 	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -115,6 +115,25 @@
 				<a href="###"><i class="glyphicon glyphicon-comment ml10 mr5"></i>3333</a>
 			</div>
 		</article>
+		<div class="page">
+			<ul class="pagination pagination-lg">
+			    <li>
+			      <a href="#" aria-label="Previous">
+			        <span aria-hidden="true">&laquo;</span>
+			      </a>
+			    </li>
+			    <li><a href="#">1</a></li>
+			    <li><a href="#">2</a></li>
+			    <li><a href="#">3</a></li>
+			    <li><a href="#">4</a></li>
+			    <li><a href="#">5</a></li>
+			    <li>
+			      <a href="#" aria-label="Next">
+			        <span aria-hidden="true">&raquo;</span>
+			      </a>
+			    </li>
+			  </ul>
+		</div>
 	</div>
 	<div class="col-md-3">Cd</div>
 </div>
