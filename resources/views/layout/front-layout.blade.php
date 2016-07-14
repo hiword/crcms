516960bb07d8b1cd31783b4b0a744aa78194bd41
hiword <28737164@qq.com>
2016-07-14 08:44:29 +0800

    style

--- resources/views/layout/front-layout.blade.php
+++ resources/views/layout/front-layout.blade.php
@@ -4,9 +4,10 @@
 	<meta charset="UTF-8" />
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	@section('meta')
-	<title>{{config('site.web_name')}}</title>
-	@show
+	<title>
+	@yield('title',config('site.web_name'))
+	</title>
+	@yield('meta')
 	<link rel="stylesheet" href="{{static_asset('vendor/bootstrap/css/bootstrap.min.css')}}" />
 	<link rel="stylesheet" href="{{static_asset('vendor/common/css/global.css')}}" />
 	<link rel="stylesheet" href="{{static_asset('vendor/common/css/bootstrap.custom.css')}}" />
