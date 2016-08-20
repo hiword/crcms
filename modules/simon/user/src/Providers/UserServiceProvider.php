250708120b2170cf52640bf3b9df67f69f02a2ce
simon <28737164@qq.com>
2016-08-20 20:07:48 +0800

    modules

---
+++ modules/simon/user/src/Providers/UserServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simon
+ * Date: 2016/8/20
+ * Time: 20:01
+ */
+namespace Simon\User\Providers;
+use Illuminate\Support\ServiceProvider;
+
+
+
+class UserServiceProvider extends ServiceProvider
+{
+
+
+
+}
