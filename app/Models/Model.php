638b5a8a043aeb60f9257e2ec650a4faf255a510
simon <28737164@qq.com>
2016-08-21 13:21:55 +0800

    basic model

---
+++ app/Models/Model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simon
+ * Date: 2016/8/21
+ * Time: 13:15
+ */
+
+namespace App\Models;
+
+
+abstract class Model extends \Illuminate\Database\Eloquent\Model
+{
+
+}
