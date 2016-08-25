97a26f62df1e99b190d3f9c5eefac4753df3b6d2
hiword <28737164@qq.com>
2016-08-25 16:28:20 +0800

    basic Service

---
+++ app/Services/AppService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simon
+ * Date: 2016/8/25
+ * Time: 16:20
+ */
+
+namespace App\Services;
+
+
+
+abstract class AppService
+{
+
+    protected $repository = null;
+
+}
