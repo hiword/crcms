d6f42a6557f15b91ec9d8376ca0416f5798e8ce0
hiword <28737164@qq.com>
2016-08-25 16:28:58 +0800

    auth其它常用服务

---
+++ modules/simon/user/src/Services/Interfaces/AuthInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simon
+ * Date: 2016/8/25
+ * Time: 16:11
+ */
+
+namespace Simon\User\Services\Interfaces;
+
+
+interface AuthInterface
+{
+
+    public function mailVerify();
+
+}
