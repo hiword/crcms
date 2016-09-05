67b252850365202ec3c107964d163d40a77f5203
hiword <28737164@qq.com>
2016-09-05 17:15:01 +0800

    add scopeStatus

--- modules/simon/acl/src/Models/AclAppAuthorize.php
+++ modules/simon/acl/src/Models/AclAppAuthorize.php
@@ -9,6 +9,7 @@
 namespace Simon\Acl\Models;
 
 
+use Illuminate\Database\Eloquent\Builder;
 use Simon\Kernel\Models\Model;
 use Simon\Kernel\Models\Traits\SoftDeletes;
 
@@ -16,5 +17,11 @@ class AclAppAuthorize extends Model
 {
 
     use SoftDeletes;
+
+
+    public function scopeStatus(Builder $query,$status)
+    {
+        return $query->where('status',$status);
+    }
 
 }
